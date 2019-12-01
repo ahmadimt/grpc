5daf5e4bd448df826cc1fa3e709b900ff8f126da
Ashish Waghmare <awaghmare@taranawireless.com>
2019-12-01 12:00:03 +0530

    clean up build scripts

--- grpc_client/src/main/java/com/imti/GnmiClient.java
+++ grpc_client/src/main/java/com/imti/GnmiClient.java
@@ -1,16 +1,37 @@
 package com.imti;
 
+import gnmi.Gnmi.SubscribeResponse;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.stub.StreamObserver;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class GnmiClient {
 
+  private static final Logger logger = Logger.getLogger(GnmiClient.class.getName());
+
   public static void main(String[] args) {
-      ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8080)
-          .usePlaintext(true).build();
+    ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8080)
+        .usePlaintext(true).build();
 
-      new StreamObserver<SubscribeRequest>()
-    }
+    new StreamObserver<SubscribeResponse>() {
 
+      @Override
+      public void onNext(final SubscribeResponse value) {
+
+      }
+
+      @Override
+      public void onError(final Throwable t) {
+        logger.log(Level.WARNING, "error cancelled");
+      }
+
+      @Override
+      public void onCompleted() {
+        //TODO
+      }
+    };
   }
+
+}
