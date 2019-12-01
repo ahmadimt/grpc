fc0af6135ace9eae39e9c903fbac79d53284a54d
Ashish Waghmare <ashishwaghmare@gmail.com>
2019-12-01 15:07:16 +0530

    fixing multi builds

--- grpc_client/src/main/java/com/imti/GnmiClient.java
+++ grpc_client/src/main/java/com/imti/GnmiClient.java
@@ -1,8 +1,15 @@
 package com.imti;
 
+import com.imti.helloworld.GreeterGrpc;
+import com.imti.helloworld.HelloRequest;
+import com.imti.helloworld.HelloResponse;
 import gnmi.Gnmi.SubscribeResponse;
+import gnmi.gNMIGrpc;
+import gnmi.gNMIGrpc.gNMIBlockingStub;
+import gnmi.gNMIGrpc.gNMIStub;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
 import io.grpc.stub.StreamObserver;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -12,26 +19,30 @@ public class GnmiClient {
   private static final Logger logger = Logger.getLogger(GnmiClient.class.getName());
 
   public static void main(String[] args) {
-    ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8080)
-        .usePlaintext(true).build();
+    ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8989).usePlaintext()
+        .build();
+    gNMIStub gNMIStub = gNMIGrpc.newStub(channel);
 
-    new StreamObserver<SubscribeResponse>() {
-
+    final StreamObserver<SubscribeResponse> responseObserver = new StreamObserver<SubscribeResponse>() {
       @Override
       public void onNext(final SubscribeResponse value) {
-
+        logger.info(value.toString());
       }
 
       @Override
       public void onError(final Throwable t) {
-        logger.log(Level.WARNING, "error cancelled");
+        logger.severe(t.toString());
       }
 
       @Override
       public void onCompleted() {
-        //TODO
+        logger.info("Completed");
       }
     };
+
+    gNMIStub.subscribe(responseObserver);
+    while (true);
+
   }
 
 }
